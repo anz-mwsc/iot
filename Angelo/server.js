e8c82f6d4760346aed43c2c12ad2905bbc1ad12f
Hinata Nakamoto <NAK0010@mwsc.vic.edu.au>
2019-06-06 16:02:23 +1000

    Play Game

--- Angelo/server.js
+++ Angelo/server.js
@@ -19,5 +19,17 @@ app.get('/shutdown', function (req, res) {
     });
 })
 
+app.get('/playmariokart', function (req, res) {
+    res.send("Starting Mario Kart");
+
+    exec('/opt/retropie/supplementary/runcommand/runcommand.sh 0 _SYS_ n64 /home/pi/RetroPie/roms/n64/MarioKart.z64', function (error, stdout, stderr) {
+        console.log('stdout: ' + stdout);
+        console.log('stderr: ' + stderr);
+        if (error !== null) {
+            console.log('exec error: ' + error);
+        }
+    });
+})
+
 
 app.listen(80);
