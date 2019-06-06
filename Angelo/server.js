8b2d24fb5dfb69faf323c4e294c3225bea504be5
Angelo Perera <angelo.perera@anz.com>
2019-06-06 15:28:16 +1000

    Updated server.js to shutdown computer at /shutdown

--- Angelo/server.js
+++ Angelo/server.js
@@ -1,6 +1,18 @@
 var express = require('express');
+const { exec } = require('child_process');
+
+
 var app = express();
 
 app.use('/', express.static('website'));
+
+
+app.get('/shutdown', function (req, res) {
+    res.send("Shutting down!");
+
+    exec('shutdown', function (err, stdout, stderr) {
+    });
+})
+
 
 app.listen(80);
