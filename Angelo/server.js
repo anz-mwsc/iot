f7c9a446d694dd9d07dac08f7d6713da07f35a5f
Angelo Perera <angelo.perera@anz.com>
2019-06-06 15:31:23 +1000

    Added logging for the shutdown command

--- Angelo/server.js
+++ Angelo/server.js
@@ -10,7 +10,12 @@ app.use('/', express.static('website'));
 app.get('/shutdown', function (req, res) {
     res.send("Shutting down!");
 
-    exec('shutdown', function (err, stdout, stderr) {
+    exec('shutdown', function (error, stdout, stderr) {
+        sys.print('stdout: ' + stdout);
+        sys.print('stderr: ' + stderr);
+        if (error !== null) {
+            console.log('exec error: ' + error);
+        }
     });
 })
 
